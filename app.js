555853ba6e2e7ff95ab20576f492f855269d7a9b
Demonik993 <wiecek.dom@gmail.com>
2023-09-05 20:06:18 +0200

    show results to user and closin buttons

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ const questionTemplate = document.querySelector('[question-form-template]');
 
 let questionNumber;
 let testName;
+function closeTest() {
+    const container = document.querySelector("#test-container");
+    container.remove();
+}
+//show results to user
+function showAnswers(results){
+    questionForm.innerHTML = "";
+    questionForm.style = "display:block; height:fit-content; width:fit-content; padding: 20px"
+    for(let data of Object.entries(results)){
+        const quest = document.createElement('h3');
+        const para = document.createElement('p');
+        para.style = "justify-content: flex-start; padding-left:20px"
+        quest.textContent = `${data[0]}: ${data[1][0]}`
+        if(data[1][1]==="correct answer"){para.textContent=`You are right, answer is: ${data[1][2]}.`}
+        if(data[1][1]==="wrong answer"){para.textContent=`Your answer is ${data[1][2]}, correct answer is: ${data[1][3]} (${data[1][4]}).`}
+        if(data[1][1]==="no answer"){para.textContent=`You didn't give any answer, correct answer is: ${data[1][2]} (${data[1][3]}).`}
+        questionForm.appendChild(quest);
+        questionForm.appendChild(para);
+    }
+    //append closing button
+    const closeResults = document.createElement('button');
+    closeResults.className = "button";
+    closeResults.textContent ="I've got it. Finish the test";
+    closeResults.addEventListener('click', ()=>{closeTest()});
+    questionForm.appendChild(closeResults);
+
+}
+
 //calculate and send answers
 function calculateAnswers(answers, questions){
     questionForm.innerHTML = "";
@@ -18,7 +46,7 @@ function calculateAnswers(answers, questions){
         email: userAnswers.student.email,
         answers: []
     };
-    console.log(userAnswers);
+    const showResults = {};
     for(let i=1; i<=5; i++){
         if(answers[i][0]===userAnswers[i]){
             correctAnswers++;
@@ -33,6 +61,7 @@ function calculateAnswers(answers, questions){
                 case "D": answer = questions[i][4];
             };          
             checkedAnswers.answers.push(`On question ${i}: ${questions[i][0]} student gave correct answer:${userAnswers[i]}-${answer}.`);
+            showResults[`Question ${i}`] = [questions[i][0], "correct answer", answer];
         }else if(userAnswers[i]!=="no answer" && answers[i][0]!==userAnswers[i]){
             wrongAnswers++;
             let correctAnswer;
@@ -55,26 +84,28 @@ function calculateAnswers(answers, questions){
                 break;
                 case "D": answer = questions[i][4];
             };          
-            checkedAnswers.answers.push(`On question ${i}: ${questions[i][0]} student gave wrong answer:${userAnswers[i]}-${answer}. Correct answer ${answers[i][0]}-${correctAnswer} (${questions[i][1]})`);
+            checkedAnswers.answers.push(`On question ${i}: ${questions[i][0]} student gave wrong answer:${userAnswers[i]}-${answer}. Correct answer ${answers[i][0]}-${correctAnswer} (${answers[i][1]})`);
+            showResults[`Question ${i}`] = [questions[i][0], "wrong answer", answer, correctAnswer, answers[i][1]];
         } else if (userAnswers[i]==="no answer"){
             noAnswers++
+            let correctAnswer;
+            switch(answers[i][0]){
+                case "A": correctAnswer = questions[i][1]; 
+                break;
+                case "B": correctAnswer = questions[i][2]; 
+                break;
+                case "C": correctAnswer = questions[i][3]; 
+                break;
+                case "D": correctAnswer = questions[i][4];
+            };          
             checkedAnswers.answers.push(`On question ${i}: ${questions[i][0]} student didn't give any answer!`)
+            showResults[`Question ${i}`] = [questions[i][0], "no answer", correctAnswer, answers[i][1]];
         }
     };
     console.log(checkedAnswers);
     questionForm.style.justifyContent = "normal";
     questionForm.style.flexDirection = "column";
     questionForm.style.alignContent = "center";
-    /*
-    for(let keyAnswer of Object.entries(answers)){for(let answer of Object.entries(userAnswers)){
-            if(keyAnswer[0]===answer[0] && keyAnswer[1][0] === answer[1]){
-                correctAnswers++;
-            } else if( keyAnswer[0] === answer[0] && answer[1]!=="no answer" && keyAnswer[1][0]!==answer[1]){
-                wrongAnswers++;
-            } else if(keyAnswer[0] === answer[0] && answer[1] === "no answer"){
-                noAnswers++;
-    }}};
-    */
 
     const result = Math.round((correctAnswers/ /*Object.keys(answers).length)*/5*100))/100;
 
@@ -87,8 +118,7 @@ function calculateAnswers(answers, questions){
     }, function(error) {
        console.log('FAILED...', error);
     });
-    //PREPARE BUTTON AND FUNCTION SHOW MY ANSWERS
-    //
+    
     questionForm.innerHTML =`
     <h2>Congratulations you have finished your test!</h2>
     <ul>
@@ -97,8 +127,18 @@ function calculateAnswers(answers, questions){
         <li> You didn't response on ${noAnswers} questions.</li>
         <li> Your result: ${result*100}%</li>
     </ul>
+    <p>
+        <button class="button" id="show-answers"> Show my answers </button>
+        <button class="button" id="finish-test"> Finish the test </button>
+    </p>
     `
-    console.log(checkedAnswers)
+    // BUTTON SHOW MY ANSWERS
+    const showMeAnswer = document.querySelector('#show-answers');
+    showMeAnswer.addEventListener('click', ()=>{showAnswers(showResults)});
+    //BUTTON TO DELETE ALL TEST DIV (TO USE IT ON OTHER WEBSITE)
+    const close = document.querySelector("#finish-test");
+    close.addEventListener('click', ()=> {closeTest()});
+
 }
 
 //add form with question
@@ -108,9 +148,7 @@ function nextQuestion(questionNumber, questions){
     const progress = document.querySelector("#progress");
     const width = Math.round((questionNumber/5/*Object.keys(questions).length ?*/)*100)/100;
     progress.style.width = `${width*(progressBar.offsetWidth)}px`;
-
-
-    
+   
     const question = questionTemplate.content.cloneNode(true).children[0];
     const legend = question.querySelector('legend');
     const questionVal = question.querySelector('[questionText]');
@@ -169,9 +207,6 @@ function nextQuestion(questionNumber, questions){
             .then (json => calculateAnswers(json, questions))
         }
     };
-
-
-
 };
 
 //apply questions
@@ -206,4 +241,3 @@ form.onsubmit = async (e) =>{
     loadQuestion(questionNumber);
 
 }
-console.log(userAnswers)
